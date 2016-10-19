6311bdc39fe9e685a7f38e8fea46182fae5f54db
Divya Mahendran <dmahendran@Divyas-Macbook-Pro.local>
2016-10-19 16:23:00 -0400

    fi

--- app.js
+++ app.js
@@ -16,11 +16,8 @@ app.post('/webhook', function (req, res) {
         req.on('end', function () {
             console.log("params" + JSON.parse(jsonString));
             postParams = JSON.parse(jsonString);
+            console.log(postparams.result.parameters.filterAttributes);
         });
-
-        
-        console.log(postparams.result.parameters.filterAttributes);
-
     }
 
   res.contentType('application/json');
