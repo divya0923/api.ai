7c455225ec9b0a34894f4858cc39fee8c712054f
Divya Mahendran <mdivyaa.23@gmail.com>
2017-05-18 12:35:30 -0400

    code cleanup

--- app.js
+++ app.js
@@ -58,13 +58,6 @@ app.post('/webhook', function (req, res) {
         any = JSON.parse(jsonString).result.parameters.any;
         contextName = JSON.parse(jsonString).result.contexts[0].name;
         
-        // TODO - fix this log 
-        //console.log("postParam: " + postParam + "name :" + contextName);
-
-        /*if(brand != null || any != null){
-           gotoSatFlow(jsonString, req, res);
-        }*/
-
         if(action == "searchFilterWithAttr") {
           searchFilterWithAttribute(jsonString, req, res);
         }
@@ -103,117 +96,7 @@ app.post('/webhook', function (req, res) {
 
         else if(action == "searchBrandWithCriteriaWithoutBrand") {
           searchBrandWithCriteria(jsonString, req, res);
-        }
-  
-        // TODO - change else to actions 
-        // if param is null, send default value as response 
-        else if(postParam == null){
-            console.log("post param null, get default filter flow");
-
-            filter.view('searchFilterDesign', 'attributesRatingView', function(err, body) {   
-              if (!err) {
-
-                // get attribute rating from the response
-                var attributes = body.rows[0].value;
-
-                // read the previous priority 
-                var prevContext = localStorage.getItem("prevContext");
-                console.log("prevContext" + prevContext);
-                // read priority from local storage
-                var priority = localStorage.getItem("filterPriority");
-                
-                if(prevContext == contextName){
-                   priority = parseInt(priority) + 1;
-                }
-                else {
-                  priority = 0;
-                }
-
-                if(priority > body.rows[0].value.length - 1 || priority < 0){
-                  console.log("invalid value for priority, defaulting it to 1");
-                  priority = 0;
-                }
-
-                localStorage.setItem("filterPriority", priority);
-
-                console.log("priority" + priority);
-
-                // sort based on the attribute priority 
-                attributes.sort(function(a, b) { 
-                  return parseFloat(a.priority) - parseFloat(b.priority);
-                });
-
-                console.log("attribute with highest priority: " +attributes[priority].name);
-                
-                filter.view('searchFilterDesign', 'searchFilterView', function(err, body) {
-                  if (!err) { 
-                    // get the array of filters from response
-                    var filterRows = body.rows;
-                    
-                    // sort rows based on the request param
-                    filterRows.sort(function(a, b) {
-                      return parseFloat(a.value[attributes[priority].name]) - parseFloat(b.value[attributes[priority].name]);
-                    });
-
-                    console.log("matching filter model:" +filterRows[0].value.filterModel);
-                    
-                    // construct response object
-                    response =  {
-                      "speech": "Okay, other customers have felt that " + attributes[priority].displayName + " is one of the important things to consider when buying an air filter. " + attributes[priority].desc + " In this store, " + filterRows[0].value.filterModel + " is the best air filter for " + attributes[priority].displayName + " based on customer review and industry data. Would you like to purchase " + filterRows[0].value.filterModel +"?",
-                      "displayText": "Filter matching your query is " + filterRows[0].value.filterModel,
-                      "source": "apiai-filter-search"
-                    };
-
-                    localStorage.setItem("prevContext", contextName);
-                    // post response
-                    res.contentType('application/json');
-                    res.send(response); 
-                  }
-                });
-              }
-            });
-        }
-
-        // else fetch results for the user defined attribute 
-        else {
-          console.log("post param not null");
-
-          // query data from couchDB through views  
-          filter.view('searchFilterDesign', 'searchFilterView', function(err, body) {
-          if (!err) { 
-            // get the array of filters from response
-            var filterRows = body.rows;
-            
-
-            // sort rows based on the request param
-            filterRows.sort(function(a, b) {
-              return parseFloat(a.value[postParam]) - parseFloat(b.value[postParam]);
-            });
-
-            console.log("matching filter model:" +filterRows[0].value.filterModel);
-            
-            if(filterRows[0].value.hasOwnProperty(postParam))           
-              response =  {
-                "speech": "Great, I can help you with that. In this store, " + filterRows[0].value.filterModel + " is the best air filter for " + postParam + " based on customer review and industry data. Would you like to purchase " + filterRows[0].value.filterModel +"?" ,
-                "displayText": "Filter matching your query is " + filterRows[0].value.filterModel,
-                "source": "apiai-filter-search"
-              };
-            else
-              // TODO - fix this error message
-              response =  {
-                "speech": "I am sorry. I did not recognize what you said. Can you please describe it in another way?",
-                "displayText": "Filter not found",
-                "source": "apiai-filter-search"
-              };
-
-            localStorage.setItem("prevContext", contextName);
-
-            // post response
-            res.contentType('application/json');
-            res.send(response); 
-          }
-          });
-        }         
+        }  
     });
   }
 });
